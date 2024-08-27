4e4fb90fc0bec43724a3cafeb6161f181680cb63
Paulo Diego <pdiegodo@gmail.com>
2024-08-26 23:24:06 -0300

    Fix de notificações, ajustado para listas individual de erro e mensagem

--- QualEaResposta.Infrastructure/Services/NotificationService.cs
+++ QualEaResposta.Infrastructure/Services/NotificationService.cs
@@ -5,43 +5,61 @@
     /// </summary>
     public class NotificationService : INotificationService, INotificador
     {
-        private readonly List<Notificacao> _notificacoes = [];
+        private readonly List<Notificacao> _notificacoesErro = new(); // Lista para notificações de erro
+        private readonly List<Notificacao> _notificacoesMensagem = new(); // Lista para notificações de mensagens de sucesso
 
         public void NotificarErro(string mensagem)
         {
-            Handle(new Notificacao(mensagem));
+            HandleErro(new Notificacao(mensagem));
         }
 
         public void NotificarMensagem(string mensagem)
         {
-            Handle(new Notificacao(mensagem, ETipoNotificacao.Mensagem));
+            HandleMensagem(new Notificacao(mensagem, ETipoNotificacao.Mensagem));
         }
 
         public bool OperacaoValida()
         {
-            return _notificacoes.Count == 0;
+            // Só considera a operação inválida se houver notificações de erro
+            return _notificacoesErro.Count == 0;
         }
 
         public List<string> ObterMensagens()
         {
-            return _notificacoes.Select(n => n.Mensagem).ToList();
+            // Retorna tanto as mensagens de erro quanto de sucesso
+            return _notificacoesErro.Concat(_notificacoesMensagem)
+                                    .Select(n => n.Mensagem)
+                                    .ToList();
         }
 
         // Implementação de métodos da interface INotificador
 
         public bool TemNotificacao()
         {
-            return _notificacoes.Count != 0;
+            return _notificacoesErro.Count != 0 || _notificacoesMensagem.Count != 0;
         }
 
         public List<Notificacao> ObterNotificacoes()
         {
-            return _notificacoes;
+            return _notificacoesErro.Concat(_notificacoesMensagem).ToList();
+        }
+
+        private void HandleErro(Notificacao notificacao)
+        {
+            _notificacoesErro.Add(notificacao);
+        }
+
+        private void HandleMensagem(Notificacao notificacao)
+        {
+            _notificacoesMensagem.Add(notificacao);
         }
 
         public void Handle(Notificacao notificacao)
         {
-            _notificacoes.Add(notificacao);
+            if (notificacao.Tipo == ETipoNotificacao.Erro)
+                HandleErro(notificacao);
+            else
+                HandleMensagem(notificacao);
         }
     }
 }
